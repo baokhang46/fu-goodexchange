9d1f113e7dd1d3452cca6f6567a56984427d08b5
tuan-anh-83 <117378521+tuan-anh-83@users.noreply.github.com>
2024-06-12 23:57:28 +0700

    Seller Base

---
+++ Services/SellerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BussinessObject.Model;
+using Repositories;
+
+namespace Services
+{
+	public class SellerService : ISellerService
+	{
+		private ISellerRepostory sellerRepostory = null;
+		public void AddSeller(Seller seller)
+		{
+			sellerRepostory.AddSeller(seller);
+		}
+
+		public void DeleteSeller(int seller)
+		{
+			sellerRepostory.DeleteSeller(seller);
+		}
+
+		public List<Seller> GetAllSeller()
+		{
+			return sellerRepostory.GetAllSeller();
+		}
+
+		public Seller GetSellerByID(int seller)
+		{
+			return sellerRepostory.GetSellerByID(seller);
+		}
+
+		public void UpdateSeller(Seller seller)
+		{
+			sellerRepostory.UpdateSeller(seller);
+		}
+	}
+}
