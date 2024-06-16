d24b66015363dd18b67a67d35bbe8378ae3e0824
baokhang46 <144251046+baokhang46@users.noreply.github.com>
2024-06-16 23:49:32 +0700

    Add Home Page

---
+++ FUGoodsExchange/Pages/SellerHomePage/SellerHomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUGoodsExchange.Pages.SellerHomePage
+{
+    public class SellerHomePaageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
