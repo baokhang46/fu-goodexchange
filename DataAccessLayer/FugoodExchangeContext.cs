e0f9ab2142235ff6eed3e0b38f31119d3d79159b
baokhang46 <144251046+baokhang46@users.noreply.github.com>
2024-06-16 23:27:12 +0700

    Update login and layout

--- DataAccessLayer/FugoodExchangeContext.cs
+++ DataAccessLayer/FugoodExchangeContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BussinessObject.Model;
 
@@ -38,8 +39,15 @@ public partial class FugoodexchangeContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=123456;database=FUgoodexchange;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
+
+    string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json").Build();
+        return config["ConnectionStrings:DefaultConnectionString"];
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
