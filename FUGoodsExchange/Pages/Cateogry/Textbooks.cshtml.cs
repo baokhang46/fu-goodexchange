f75978fff3272eef754ce85fa53ce2261b436e0b
nguyenquangbaokhang <khangnqbse171663@fpt.edu.vn>
2024-06-17 13:47:47 +0700

    Update layout

---
+++ FUGoodsExchange/Pages/Cateogry/Textbooks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUGoodsExchange.Pages.Cateogry
+{
+    public class TextBooksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
